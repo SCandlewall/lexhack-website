fc4d5a8f8a9d7910d9e86b6c5b33ae95640d8e74
Luke West <l.s.west88@gmail.com>
2016-07-18 11:38:51 -0400

    testing new way to deal with fixed position on mobile

--- js/menu.js
+++ js/menu.js
@@ -1,25 +1,34 @@
 window.onload = function() {
-	var sticked = false;
+	if(!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) ) {
+		var sticked = false;
 
-	if ((top < 0)){
-		menu.style.position = "fixed";
-		sticked = true;
-	}
-
-
-	window.onscroll = function() {
-		var placeholder = document.getElementById("placeholder");
-		var menu = document.getElementById("menu");
-		var top = placeholder.getBoundingClientRect().top;
-
-		if ((top < 0) && (sticked == false)){
+		if ((top < 0)){
 			menu.style.position = "fixed";
 			sticked = true;
 		}
 
-		if ((top >= 1) && (sticked == true)){
-			menu.style.position = "relative";
-			sticked = false;
+
+		window.onscroll = function() {
+			var placeholder = document.getElementById("placeholder");
+			var menu = document.getElementById("menu");
+			var top = placeholder.getBoundingClientRect().top;
+
+			if ((top < 0) && (sticked == false)){
+				menu.style.position = "fixed";
+				sticked = true;
+			}
+
+			if ((top >= 1) && (sticked == true)){
+				menu.style.position = "relative";
+				sticked = false;
+			}
 		}
+	}
+
+	else {
+		var menu = document.getElementById("menu");
+		menu.style.position = "fixed";
+		menu.removeAttribute("top");
+		menu.style.bottom = 0;
 	}
 }
